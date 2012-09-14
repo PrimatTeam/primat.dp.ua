cf783497e66427d59cdbe435f2941b331388099d
pesua <pesua.mail@gmail.com>
2012-09-14 08:38:33 +0300

    Adding groupFlag to StudentGroup

--- Curriculum/src/main/java/ua/dp/primat/domain/StudentGroup.java
+++ Curriculum/src/main/java/ua/dp/primat/domain/StudentGroup.java
@@ -2,6 +2,7 @@ package ua.dp.primat.domain;
 
 import java.io.Serializable;
 import java.text.DecimalFormat;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -9,13 +10,14 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
+
 import org.apache.commons.lang.builder.EqualsBuilder;
 import org.apache.commons.lang.builder.HashCodeBuilder;
 
 @Entity
 @NamedQueries({
-    @NamedQuery(name = StudentGroup.GET_GROUPS_QUERY, query = "select n from StudentGroup n order by n.code, n.year, n.number"),
-    @NamedQuery(name = StudentGroup.GET_GROUPS_BY_CODE_AND_YEAR_AND_NUMBER_QUERY, query = "select n from StudentGroup n where n.code = :code and n.year = :year and n.number = :number")
+        @NamedQuery(name = StudentGroup.GET_GROUPS_QUERY, query = "select n from StudentGroup n order by n.code, n.year, n.number"),
+        @NamedQuery(name = StudentGroup.GET_GROUPS_BY_CODE_AND_YEAR_AND_NUMBER_QUERY, query = "select n from StudentGroup n where n.code = :code and n.year = :year and n.number = :number")
 })
 public class StudentGroup implements Serializable {
 
@@ -23,7 +25,17 @@ public class StudentGroup implements Serializable {
     public static final String GET_GROUPS_BY_CODE_AND_YEAR_AND_NUMBER_QUERY = "getGroupsByCodeAndYearAndNumber";
     public static final int CODE_LENGTH = 2;
 
+    private static final Pattern CODE_PATTERN = Pattern.compile("^(.+?)-");
+    private static final Pattern YEAR_PATTERN = Pattern.compile("-(\\d{2})");
+    private static final Pattern TYPE_PATTERN = Pattern.compile("\\d{2}([а-яa-z]?)");
+    private static final Pattern NUMBER_PATTERN = Pattern.compile("-(\\d)$");
+
     public StudentGroup() {
+    }
+
+    public StudentGroup(String code, Long number, Long year, String groupType) {
+        this(code, number, year);
+        this.groupType = groupType;
     }
 
     public StudentGroup(String code, Long number, Long year) {
@@ -33,19 +45,27 @@ public class StudentGroup implements Serializable {
     }
 
     public StudentGroup(String fullCode) {
-        if (!Pattern.matches("\\D{2,5}-\\d{2,4}-\\d{1,}", fullCode)) {
+        if (!Pattern.matches("\\D{2}\\-\\d{2}[а-я\\w]?(-\\d)?", fullCode)) {
             throw new IllegalArgumentException("Wrong student group code");
         }
 
-        try {
-            final char separator = '-';
-            final int firstDefis = fullCode.indexOf(separator);
-            final int secondDefis = fullCode.indexOf(separator, firstDefis + 1);
-            this.code = fullCode.substring(0, firstDefis);
-            this.year = YEARBASE + Long.parseLong(fullCode.substring(firstDefis + 1, secondDefis));
-            this.number = Long.parseLong(fullCode.substring(secondDefis + 1));
-        } catch (IndexOutOfBoundsException e) {
-            throw new IllegalArgumentException(e);
+        Matcher codeMatcher = CODE_PATTERN.matcher(fullCode);
+        codeMatcher.find();
+        code = codeMatcher.group(1);
+
+        Matcher yearMatcher = YEAR_PATTERN.matcher(fullCode);
+        yearMatcher.find();
+        year = YEARBASE + Long.valueOf(yearMatcher.group(1));
+
+        Matcher typeMatcher = TYPE_PATTERN.matcher(fullCode);
+        typeMatcher.find();
+        groupType = typeMatcher.group(1);
+
+        Matcher numberMatcher = NUMBER_PATTERN.matcher(fullCode);
+        if (numberMatcher.find()) {
+            number = Long.valueOf(numberMatcher.group(1));
+        } else {
+            number = 1L;
         }
     }
 
@@ -81,6 +101,14 @@ public class StudentGroup implements Serializable {
         this.year = year;
     }
 
+    public String getGroupType() {
+        return groupType;
+    }
+
+    public void setGroupType(String groupType) {
+        this.groupType = groupType;
+    }
+
     @Override
     public boolean equals(Object obj) {
         if (obj == null) {
@@ -114,6 +142,7 @@ public class StudentGroup implements Serializable {
         final String yearCode = format.format(getYear() % yearMask);
         return String.format("%s-%s-%d", getCode(), yearCode, getNumber());
     }
+
     private static final Long YEARBASE = 2000L;
     private static final long serialVersionUID = 1L;
     @Id
@@ -123,4 +152,5 @@ public class StudentGroup implements Serializable {
     private String code;
     private Long number;
     private Long year;
+    private String groupType;
 }
--- Curriculum/src/test/java/ua/dp/primat/domain/StudentGroupTest.java
+++ Curriculum/src/test/java/ua/dp/primat/domain/StudentGroupTest.java
@@ -8,10 +8,10 @@ package ua.dp.primat.domain;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
- *
  * @author EniSh
  */
 public class StudentGroupTest {
@@ -29,11 +29,31 @@ public class StudentGroupTest {
 
     @Test
     public void testCreationGroupByFullCode() {
-        System.out.println("CreationGroupByFullCode");
         StudentGroup instance = new StudentGroup("PZ-08-1");
 
         assertEquals("PZ", instance.getCode());
         assertEquals(Long.valueOf(2008), instance.getYear());
         assertEquals(Long.valueOf(1), instance.getNumber());
+        assertEquals("", instance.getGroupType());
+    }
+
+    @Test
+    public void testCreationGroupByFullCode1() {
+        StudentGroup instance = new StudentGroup("ПЗ-12у");
+
+        assertEquals("ПЗ", instance.getCode());
+        assertEquals(Long.valueOf(2012), instance.getYear());
+        assertEquals(Long.valueOf(1), instance.getNumber());
+        assertEquals("у", instance.getGroupType());
+    }
+
+    @Test
+    public void testCreationGroupByFullCode2() {
+        StudentGroup instance = new StudentGroup("ПМ-12с-2");
+
+        assertEquals("ПМ", instance.getCode());
+        assertEquals(Long.valueOf(2012), instance.getYear());
+        assertEquals(Long.valueOf(2), instance.getNumber());
+        assertEquals("с", instance.getGroupType());
     }
 }
